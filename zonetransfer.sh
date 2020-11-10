265155ed7120b9e2f27a0f0b7855215be36a92b5
Lafanachy <73102259+Lafanachy@users.noreply.github.com>
2020-11-10 11:24:22 +0300

    Create zonetransfer.sh

---
+++ zonetransfer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Zone Transfer Bash Script' i
+
+if [ -z "$1" ]; then
+        echo "[*] Simple Zone Transfer Sciprt"
+        echo "[*] Usage : $0 <Domain Name>"
+        exit 0
+fi
+
+for server in $(host -t ns $1 | cut -d " " -f4); do
+        host -l $1 $server | grep "has address"
+done
